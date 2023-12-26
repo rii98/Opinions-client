ec7cc9a2bc91bf3923f403db2fa8516b7134709b
rii98 <136151503+rii98@users.noreply.github.com>
2023-12-26 11:36:08 +0545

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import "@radix-ui/themes/styles.css";
+
 import PostContextProvider from "./context/PostContext.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
