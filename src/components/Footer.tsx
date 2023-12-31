f90d6030220ef8ba52fa8251b529d6a8039622c9
rii98 <136151503+rii98@users.noreply.github.com>
2023-12-31 14:04:53 +0545

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,8 +2,10 @@ import { FaTrophy, FaUserCircle } from "react-icons/fa";
 import { IoIosAddCircle, IoIosSearch } from "react-icons/io";
 import { IoChevronBack, IoChevronForward } from "react-icons/io5";
 import { Link, useNavigate } from "react-router-dom";
+import { useAuth } from "../context/AuthContext";
 const Footer = () => {
   const navigate = useNavigate();
+  const { verified } = useAuth();
   return (
     <div>
       <div className="flex justify-center">
@@ -16,26 +18,28 @@ const Footer = () => {
           </div>
         </footer>
       </div>
-      <footer className="absolute bottom-0 gap-4 bg-blue-800 rounded-lg text-white p-4 flex left-0 right-0 justify-between md:hidden ">
-        <Link
-          to={`/profile/${localStorage.getItem("id")}`}
-          className="md:text-lg"
-        >
-          <FaUserCircle size={25} />
-        </Link>
+      {verified && (
+        <footer className="absolute bottom-0 gap-4 bg-blue-800 rounded-lg text-white p-4 flex left-0 right-0 justify-between md:hidden mb-0">
+          <Link
+            to={`/profile/${localStorage.getItem("id")}`}
+            className="md:text-lg"
+          >
+            <FaUserCircle size={25} />
+          </Link>
 
-        <Link to="/post/create" className="flex items-center">
-          <IoIosAddCircle size={30} />
-        </Link>
+          <Link to="/post/create" className="flex items-center">
+            <IoIosAddCircle size={30} />
+          </Link>
 
-        <Link to="/search" className="flex items-center">
-          <IoIosSearch size={30} />
-        </Link>
+          <Link to="/search" className="flex items-center">
+            <IoIosSearch size={30} />
+          </Link>
 
-        <Link to="/popular" className="md:text-lg">
-          <FaTrophy size={25} />
-        </Link>
-      </footer>
+          <Link to="/popular" className="md:text-lg">
+            <FaTrophy size={25} />
+          </Link>
+        </footer>
+      )}
     </div>
   );
 };
